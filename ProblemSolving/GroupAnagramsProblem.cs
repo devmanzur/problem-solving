614af84b7ff66e4f31daa119390c639a8546fce7
manzuracoreplus <manzura@coreplus.com.au>
2023-01-02 11:25:43 +0600

    leetcode-49. Group Anagrams [Solved]

---
+++ ProblemSolving/GroupAnagramsProblem.cs
@@ -0,0 +1,40 @@
+﻿namespace ProblemSolving;
+
+public class GroupAnagramsProblem
+{
+    private const int TotalLowercaseCharacters = 26;
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        if(!strs.Any()){
+            return new List<IList<string>>();     
+        }
+
+        var groups = new Dictionary<string, IList<string>>();
+        
+        foreach (var str in strs)
+        {
+            var countArray = CountCharacters(str);
+            var key = new string(countArray);
+            if (!groups.ContainsKey(key))
+            {
+                groups[key] = new List<string>();
+            }
+            groups[key].Add(str);
+        }
+        
+        return groups.Values.ToList();
+    }
+
+    private char[] CountCharacters(string str)
+    {
+        var countArray = new char[26];
+        foreach (var character in str)
+        {
+            int asciiValue = character - 'a';
+            if (asciiValue >= 0 && asciiValue < TotalLowercaseCharacters)
+            {
+                countArray[asciiValue]++;
+            }
+        }
+        return countArray;
+    }
+}
---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ProblemSolving;
+
+var problem = new GroupAnagramsProblem();
+var result = problem.GroupAnagrams(new[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+foreach (var list in result)
+{
+    Console.WriteLine("Group 1");
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+}
